911880da3c7bc43204bad3283fb8f77992f87029
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2023-03-20 14:16:12 +0530

    a sitemap

---
+++ pages/sitemap.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Sitemap = () => {
+    return (
+        <div>
+            <div>
+                <nav>mommyai.co HTML Site Map</nav>
+                <h3>
+                    <span>Last updated: 2023, March 20<br />
+                        Total pages: 1</span>
+                    <a href="http://mommyai.co">mommyai.co Homepage</a>
+                </h3>
+            </div>
+            <div>
+                <ul>
+
+                    <li>https:/ </li>
+                    <li>
+                        <ul>
+
+                            <li>/ </li>
+                            <li>
+                                <ul>
+
+                                    <li>mommyai.co/ <span class="lcount">1 pages</span></li>
+
+                                    <li><a href="https://mommyai.co/"
+                                        title="mommyAI | stories for kids">mommyAI | stories for kids</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+
+            </div>
+            <div>
+                Page created with <a target="_blank" href="https://www.xml-sitemaps.com">Google XML sitemap and html sitemaps
+                    generator</a> | Copyright &copy; 2005-2023 XML-Sitemaps.com
+            </div>
+        </div>
+    )
+}
+
+export default Sitemap
